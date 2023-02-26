5736381d223863a039a67757f95a2afa6be57dfe
Tomme-git <tommehansen@gmail.com>
2023-02-27 00:42:22 +0100

    final changes, state styling, typewriter adjustments
    hover, active, nav item active state depending on which element is currently in the viewport using Intersection Observer API, typerwriter adjustments, made it a bit faster

--- js/main.js
+++ js/main.js
@@ -5,11 +5,44 @@ let typewriter = new Typewriter(heroHeading, {
 });
 
 typewriter.typeString('Hi!')
-    .pauseFor(1500)
+    .pauseFor(900)
     .deleteAll()
     .typeString('My naem is')
     .pauseFor(300)
     .deleteChars(5)
-    .typeString('me is <span class="text-highlight">Tom</span>')
+    .typeString('me is <span class="text-highlight">Tom</span>.')
     .pause()
     .start();
+
+window.addEventListener('DOMContentLoaded', () => {
+    let nav = document.querySelectorAll(`.nav-wrapper div a`);
+
+    const options = {
+        rootMargin: '-50% 0px -50% 0px',
+    }
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                nav.forEach(link => {
+                    /*
+                    console.log(entry.target.id);
+                    console.log(link.getAttribute("href").split('#'));
+                    */
+                    if (entry.target.id === link.getAttribute("href").split('#')[1]) {
+                        link.classList.add('active');
+                    } else {
+                        link.classList.remove('active');
+                    }
+                })
+            }
+        });
+    }, options);
+
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section').forEach((section) => {
+        observer.observe(section);
+    });
+
+});
