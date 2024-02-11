bc3c3c0f786df4d245cef34c1b2d66716f987cdf
Tomme-git <tommehansen@gmail.com>
2024-02-11 17:38:07 +0100

    Levels add build

--- projects/levels/src/components/SubpageHeroText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SubpageHeroText(props) {
-  return (
-    <div className='low-opacity-text'>
-        <h1><span className='span-white'>{props.whiteText}</span>{props.greyText}</h1>
-    </div>
-  )
-}
