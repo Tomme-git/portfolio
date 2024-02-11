5cefd68f270e4eb7bf798dc76adafeebc9f64feb
Tomme-git <tommehansen@gmail.com>
2024-02-11 05:24:24 +0100

    gh-pages

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 let heroHeading = document.getElementById('typewriter');
 
 let typewriter = new Typewriter(heroHeading, {
-    loop: false
+  loop: false
 });
 
 typewriter.typeString('Hi!')
-    .pauseFor(900)
-    .deleteAll()
-    .typeString('My naem is')
-    .pauseFor(300)
-    .deleteChars(5)
-    .typeString('me is <span class="text-highlight">Tom</span>.')
-    .pause()
-    .start();
+  .pauseFor(900)
+  .deleteAll()
+  .typeString('My naem is')
+  .pauseFor(300)
+  .deleteChars(5)
+  .typeString('me is <span class="text-highlight">Tom</span>.')
+  .pause()
+  .start();
 
 window.addEventListener('DOMContentLoaded', () => {
-    let nav = document.querySelectorAll(`.nav-wrapper div a`);
+  let nav = document.querySelectorAll(`.nav-wrapper div a`);
 
-    const options = {
-        rootMargin: '-50% 0px -50% 0px',
-    }
+  const options = {
+    rootMargin: '-50% 0px -50% 0px',
+  }
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
 
-            if (entry.isIntersecting) {
-                nav.forEach(link => {
-                    /*
-                    console.log(entry.target.id);
-                    console.log(link.getAttribute("href").split('#'));
-                    */
-                    if (entry.target.id === link.getAttribute("href").split('#')[1]) {
-                        link.classList.add('active');
-                    } else {
-                        link.classList.remove('active');
-                    }
-                })
-            }
-        });
-    }, options);
-
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('section').forEach((section) => {
-        observer.observe(section);
+      if (entry.isIntersecting) {
+        nav.forEach(link => {
+          /*
+          console.log(entry.target.id);
+          console.log(link.getAttribute("href").split('#'));
+          */
+          if (entry.target.id === link.getAttribute("href").split('#')[1]) {
+            link.classList.add('active');
+          } else {
+            link.classList.remove('active');
+          }
+        })
+      }
     });
+  }, options);
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('section').forEach((section) => {
+    observer.observe(section);
+  });
 
 });
